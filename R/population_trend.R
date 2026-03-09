b57acaf7073cb3cfd39f423cde0f42fc27d0328b
petervitale910 <petervitale910@gmail.com>
2026-03-09 15:19:41 -0700

    Update function

--- R/population_trend.R
+++ R/population_trend.R
@@ -4,8 +4,9 @@ population_trends <- function(data, species_list, years_list){
   data %>% 
     st_drop_geometry() %>% 
     filter(common_name %in% species_list,
-           year_collected %in% year_list) %>% 
+           year_collected %in% years_list) %>% 
     group_by(common_name, year_collected) %>% 
-    summarise(total_obs = sum(observation_count, na.rm = TRUE)) %>% 
+    mutate(total_obs = sum(observation_count, na.rm = TRUE)) %>% 
+    ungroup() %>% 
     select(common_name, year_collected, gap_sts, habitat_type, total_obs)
 }
