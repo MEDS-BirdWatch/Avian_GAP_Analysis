f5f22c37844e4a34f8f6212dedfbf0e1b235baaa
IsabellaSegarra <irsegarra@outlook.com>
2026-06-04 17:42:47 -0700

    Add package versions.

--- environment.R
+++ environment.R
@@ -34,28 +34,28 @@
 
 # ==== Load packages ====
 
-library(sf)
-library(stars)
-library(terra)
-library(tidyterra)
-library(sfarrow)
-library(tidyverse)
-library(here)
-library(janitor)
-library(readxl)
-library(glmmTMB)
-library(broom.mixed)
-library(lmtest)
-library(spdep)
-library(indicspecies)
-library(gt)
-library(gtsummary)
-library(gtExtras)
-library(kableExtra)
-library(tmap)
-library(paletteer)
-library(patchwork)
-library(showtext)
-library(webshot2)
-library(marginaleffects)
-library(ggeffects)
+library(sf) # 1.0-24
+library(stars) # 0.6-8
+library(terra) # 1.8-60
+library(tidyterra) # 0.7.2
+library(sfarrow) # 0.4.1
+library(tidyverse)# 2.0.0
+library(here) # 1.0.2
+library(janitor) # 2.2.1
+library(readxl) # 1.4.5
+library(glmmTMB) # 1.1.13
+library(broom.mixed) # 0.2.9.6
+library(lmtest) # 0.9-40
+library(spdep) # 1.4-2
+library(indicspecies) # 1.8.0
+library(gt) # 1.2.0
+library(gtsummary) # 2.5.0
+library(gtExtras) # 0.6.1
+library(kableExtra)# 1.4.0
+library(tmap) # 4.2
+library(paletteer) # 1.7.0
+library(patchwork) # 1.3.2
+library(showtext) # 0.9-7
+library(webshot2) # 0.1.2
+library(marginaleffects) # 0.32.0
+library(ggeffects) # 2.3.2
